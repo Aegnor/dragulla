2fe13a032f045ef04089b8843c0b6cfbb6f39b67
JenkinsTheme <12alexman@gmail.com>
2020-06-29 16:12:28 +0300

    made a class Dragon

--- src/js/index.js
+++ src/js/index.js
@@ -3,107 +3,98 @@ import './assets/case'
 import gsap from "gsap"
 import getMousePosition from "./utils/dom"
 
+class Dragon {
+    constructor(elements){
 
-// мы могли это сделать и через класс
+        this.elements = typeof elements == 'string' ? document.querySelectorAll(elements) : elements;
+
+        this.isDragging = false
+        
+        this.computedPosition = { x: 0, y: 0 };
+        this.startPoint = { x: 0, y: 0 };
+        this.latestPosition = { x: 0, y: 0 };
+        this.dragDistance = { x: 0, y: 0 },
+
+        this.target,
+        this.zIndex = 0,
+
+        this.bindEvents();
+
+    }
+
+    getStylePosition( element ){
+        const style = getComputedStyle(element);
+        const x = parseInt(style.getPropertyValue('left'));
+        const y = parseInt(style.getPropertyValue('top'));
+
+        // clean up 'auto' or other non-integer values
+        this.computedPosition.x = isNaN( x ) ? 0 : x;
+        this.computedPosition.y = isNaN( y ) ? 0 : y;
+    }
+
+    onDragStart( event ){
+        if( event.button !== 0) return
+    
+        this.isDragging = true
+    
+        event.preventDefault()
+     
+        this.target = event.target;
+    
+        this.getStylePosition(this.target);
+    
+        this.startPoint.x = getMousePosition(event).x;
+        this.startPoint.y = getMousePosition(event).y;
+    
+        this.latestPosition.x = this.computedPosition.x
+        this.latestPosition.y = this.computedPosition.y
+    
+        gsap.set(this.target, {
+            zIndex: 103,
+        })
+    }
+    
+    onDragMove( event ){
+        if ( !this.isDragging ) return
+        
+        this.dragDistance.x = this.startPoint.x - getMousePosition(event).x,
+        this.dragDistance.y = this.startPoint.y - getMousePosition(event).y;
+    
+        this.latestPosition.x = -this.dragDistance.x + this.computedPosition.x;
+        this.latestPosition.y = -this.dragDistance.y + this.computedPosition.y;
+        
+        gsap.set(this.target, {
+           x: -this.dragDistance.x,
+           y: -this.dragDistance.y
+        })
+    
+    }
+    
+    onDragEnd( event ){
+        this.isDragging = false
+        
+        this.zIndex++
+
+        gsap.set(this.target, {
+            zIndex: 1 + this.zIndex,    
+            x: 0,
+            y: 0,
+            left: this.latestPosition.x,
+            top: this.latestPosition.y
+        })
+    
+    }
+    
+    bindEvents(){
+        this.elements.forEach(element => {
+            element.addEventListener('mousedown', this.onDragStart.bind(this))
+        })
+        document.addEventListener('mousemove', this.onDragMove.bind(this))
+        document.addEventListener('mouseup', this.onDragEnd.bind(this))
+    }   
+    
+}
+
 let elements = document.querySelectorAll('.item');
 
-let dragState = false
-
-let position = { x: 0, y: 0 };
-let startPoint = { x: 0, y: 0 };
-let dragPoint = { x: 0, y: 0 };
-
-let 
-    target,
-    zIndex = 0,
-    lastPosX = 0,
-    lastPosY = 0,
-    distanceX = 0,
-    distanceY = 0;
-
-function getPositionCoord( styleSide, measure ) {
-    if ( styleSide.indexOf('%') != -1 ) {
-      // convert percent into pixel for Safari, #75
-      var parentSize = getSize( this.element.parentNode );
-      // prevent not-in-DOM element throwing bug, #131
-      return !parentSize ? 0 :
-        ( parseFloat( styleSide ) / 100 ) * parentSize[ measure ];
-    }
-    return parseInt( styleSide, 10 );
-};
-
-function getStylePosition( element ){
-    var style = getComputedStyle(element);
-    var x = getPositionCoord( style.left, 'width' );
-    var y = getPositionCoord( style.top, 'height' );
-    // clean up 'auto' or other non-integer values
-    position.x = isNaN( x ) ? 0 : x;
-    position.y = isNaN( y ) ? 0 : y;
-}
-
-function onDragStart( event ){
-    if( event.button !== 0) return
-
-    dragState = true
-
-    event.preventDefault()
- 
-    target = event.target;
-
-    getStylePosition(target);
-
-    zIndex++
-
-    startPoint.x = getMousePosition(event).x;
-    startPoint.y = getMousePosition(event).y;
-
-    lastPosX = position.x
-    lastPosY = position.y
-
-    gsap.set(target, {
-        zIndex: 103,
-    })
-}
-
-function onDragMove( event ){
-    if ( !dragState ) return
-
-    dragPoint.x = getMousePosition(event).x;
-    dragPoint.y = getMousePosition(event).y;
-    
-    distanceX = startPoint.x - dragPoint.x,
-    distanceY = startPoint.y - dragPoint.y;
-
-    console.log(distanceX, distanceY)
-
-    console.log(distanceX !== 0, distanceY !== 0)
-
-    lastPosX = -distanceX + position.x;
-    lastPosY = -distanceY + position.y;
-    
-
-    gsap.set(target, {
-       x: -distanceX,
-       y: -distanceY
-    })
-
-}
-
-function onDragEnd( event ){
-    dragState = false
-
-    gsap.set(target, {
-        zIndex: 1 + zIndex,
-        x: 0,
-        y: 0,
-        left: lastPosX,
-        top: lastPosY
-    })
-
-}
-
-elements.forEach(element => {
-    element.addEventListener('mousedown', onDragStart)
-})
-document.addEventListener('mousemove', onDragMove)
-document.addEventListener('mouseup', onDragEnd)
+let drag = new Dragon(elements);
