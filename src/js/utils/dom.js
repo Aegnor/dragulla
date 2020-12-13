dbfa929c79b807e1298d180232066689a1518e63
Sbr <12alexman@gmail.com>
2020-12-13 19:16:32 +0200

    added utility functions like getTransformValues, addClass, removeClass

--- src/js/utils/dom.js
+++ src/js/utils/dom.js
@@ -1,9 +1,40 @@
-const getMousePosition = event => {
-    let 
-        x = event.clientX,
-        y = event.clientY
-    
-    return { x, y }
+export const getMousePosition = (event, isByPage = false) => {
+    if (isByPage) {
+        return {
+            x: event.pageX,
+            y: event.pageY
+        }
+    }
+
+    return {
+        x: event.clientX,
+        y: event.clientY
+    }
 }
 
-export default getMousePosition
+export const getTransformValues = selector => {
+    const style = getComputedStyle(selector)
+    const matrix = style.transform
+
+    if (matrix === 'none') {
+        return {
+            x: 0,
+            y: 0,
+        }
+    }
+
+    const matrixValues = matrix.match(/matrix.*\((.+)\)/)[1].split(', ')
+
+    return {
+        x: matrixValues[4],
+        y: matrixValues[5]
+    }
+}
+
+export const addClass = (selector, className) => {
+    selector.classList.add(className)
+}
+
+export const removeClass = (selector, className) => {
+    selector.classList.remove(className)
+}
